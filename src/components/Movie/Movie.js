dc3be16e4010f7dd211195f8ac97e1b04d1bdf97
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-04 16:11:10 -0700

    Refactor Movie to deconstruct parameters inside of function

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './Movie.css';
 
-const Movie = ({ poster_path, title, average_rating }) => (
-  <article>
-    <div className="movie-container">
-      <button
-        style={ {backgroundImage: `url(${poster_path})`} }
-        className="movie-poster"
-      >
-      </button>
-      <h2 className="movie-title">{title}</h2>
-      <p className="movie-rating">{average_rating}</p>
-    </div>
-  </article>
-)
+const Movie = ({ poster_path, title, average_rating }) => {
+  return (
+    <article>
+      <div className="movie-wrapper">
+        <button
+          style={{
+            backgroundImage: `url(${poster_path})`
+          }}
+          className="movie-poster"
+        >
+        </button>
+        <h2 className="movie-title">{title}</h2>
+        <p className="movie-rating">{average_rating}</p>
+      </div>
+    </article>
+  )
+}
 
 export default Movie;
