3d9bbb8dee5175c3fa652c013178073fb52cf07d
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-06 13:55:41 -0700

    Pass currentMovie to Details if there is one

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Movie from '../Movie/Movie.js';
+import Details from '../Details/Details.js'
 // import { Route, Switch } from 'react-router-dom';
 
 import './Container.css';
@@ -26,7 +27,7 @@ const Container = props => {
       </main>
     )
   } else {
-    return <h1>{props.currentMovie}</h1>
+    return <Details currentMovie={props.currentMovie} />
   }
 }
 
