f08aad52db43a9b1333ad0c05c6bcd38791ceedf
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-09 19:08:56 -0700

    Add background and foreground colors

--- src/components/MovieLoader/MovieLoader.js
+++ src/components/MovieLoader/MovieLoader.js
@@ -4,17 +4,29 @@ import './MovieLoader.css';
 
 const MovieLoader = () => (
   <>
-    <ContentLoader className="loader">
+    <ContentLoader
+      className="loader"
+      backgroundColor="#737373"
+      foregroundColor="#ababab"
+    >
       <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
       <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
     </ContentLoader>
-    <ContentLoader className="loader">
+    <ContentLoader
+      className="loader"
+      backgroundColor="#737373"
+      foregroundColor="#ababab"
+    >
       <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
       <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
     </ContentLoader>
-    <ContentLoader className="loader">
+    <ContentLoader
+      className="loader"
+      backgroundColor="#737373"
+      foregroundColor="#ababab"
+    >
       <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
       <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
