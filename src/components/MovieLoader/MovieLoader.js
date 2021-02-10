59bfbe015af561b6202c9f68cce274e32ee53d9c
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-09 17:21:21 -0700

    Position MovieLoaders

--- src/components/MovieLoader/MovieLoader.js
+++ src/components/MovieLoader/MovieLoader.js
@@ -3,23 +3,23 @@ import ContentLoader from 'react-content-loader';
 import './MovieLoader.css';
 
 const MovieLoader = () => (
-  <div className="movie-loader">
-    <ContentLoader>
+  <>
+    <ContentLoader className="loader">
       <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
       <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
     </ContentLoader>
-    <ContentLoader>
+    <ContentLoader className="loader">
       <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
       <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
     </ContentLoader>
-    <ContentLoader>
+    <ContentLoader className="loader">
       <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
       <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
     </ContentLoader>
-  </div>
+  </>
 )
 
 export default MovieLoader
