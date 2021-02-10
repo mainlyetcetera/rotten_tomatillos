0ce03ecb203e2f2abd8e5b65439c23c804cb1a9e
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-10 16:09:21 -0700

    Make error for user and set state with api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Header from '../Header/Header.js';
 import Container from '../Container/Container.js';
 import { Route, Switch } from 'react-router-dom';
+import fetchData from '../../apiCalls.js';
 
 export default class App extends Component {
   constructor() {
     super()
     this.state = {
       movies: [],
-      currentMovie: null
+      currentMovie: null,
+      error: false
     }
   }
 
@@ -31,6 +33,7 @@ export default class App extends Component {
               <Container
                 movies={this.state.movies}
                 updateCurrentMovie={this.updateCurrentMovie}
+                error={this.state.error}
               />
             )}
             exact
@@ -51,12 +54,13 @@ export default class App extends Component {
   }
 
   componentDidMount = () => {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
-      .then(data => setTimeout(() =>
-        this.setState({
-          movies: data.movies
-        }), 1500))
-      .catch(err => console.log(err))
+    fetchData('https://ranasdfcid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(data => {
+      console.log('data', data);
+      this.setState({
+        movies: data.movies
+      });
+    })
+    .catch(err => this.setState({error: true}));
   }
 }
