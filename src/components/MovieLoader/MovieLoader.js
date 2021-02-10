45a5902cc7e4a483ace9a2781a1719effc0c2c09
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-09 17:19:06 -0700

    Populate sample ContentLoaders

--- src/components/MovieLoader/MovieLoader.js
+++ src/components/MovieLoader/MovieLoader.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
+import './MovieLoader.css';
 
 const MovieLoader = () => (
-  <>
-    <h1>Loading</h1>
-  </>
+  <div className="movie-loader">
+    <ContentLoader>
+      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+    </ContentLoader>
+    <ContentLoader>
+      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+    </ContentLoader>
+    <ContentLoader>
+      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+    </ContentLoader>
+  </div>
 )
 
 export default MovieLoader
