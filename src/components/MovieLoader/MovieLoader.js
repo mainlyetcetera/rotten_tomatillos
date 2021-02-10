bb316851ac106fc08acbd9e3889f986fbf9a5c27
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-09 19:37:04 -0700

    Size ContentLoader to match movie poster

--- src/components/MovieLoader/MovieLoader.js
+++ src/components/MovieLoader/MovieLoader.js
@@ -4,34 +4,40 @@ import './MovieLoader.css';
 
 const MovieLoader = () => (
   <>
-    <ContentLoader
-      className="loader"
-      backgroundColor="#737373"
-      foregroundColor="#ababab"
-    >
-      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-    </ContentLoader>
-    <ContentLoader
-      className="loader"
-      backgroundColor="#737373"
-      foregroundColor="#ababab"
-    >
-      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-    </ContentLoader>
-    <ContentLoader
-      className="loader"
-      backgroundColor="#737373"
-      foregroundColor="#ababab"
-    >
-      <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-      <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-    </ContentLoader>
+    <div className="loader-wrapper">
+      <ContentLoader
+        className="loader"
+        viewBox="0 0 100 200"
+        backgroundColor="#737373"
+        foregroundColor="#ababab"
+      >
+        <rect x="5" y="0" rx="1" ry="1" width="90" height="140" />
+        <rect x="25" y="150" rx="1" ry="1" width="50" height="10" />
+      </ContentLoader>
+    </div>
+    <div className="loader-wrapper">
+      <ContentLoader
+        className="loader"
+        viewBox="0 0 100 200"
+        backgroundColor="#737373"
+        foregroundColor="#ababab"
+      >
+        <rect x="5" y="0" rx="1" ry="1" width="90" height="140" />
+        <rect x="25" y="150" rx="1" ry="1" width="50" height="10" />
+      </ContentLoader>
+    </div>
+    <div className="loader-wrapper">
+      <ContentLoader
+        className="loader"
+        viewBox="0 0 100 200"
+        backgroundColor="#737373"
+        foregroundColor="#ababab"
+      >
+        <rect x="5" y="0" rx="1" ry="1" width="90" height="140" />
+        <rect x="25" y="150" rx="1" ry="1" width="50" height="10" />
+      </ContentLoader>
+    </div>
   </>
-)
+);
 
 export default MovieLoader
