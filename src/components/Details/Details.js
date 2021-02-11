e79de497b1d34a153053587d6a06449493463654
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-10 17:36:01 -0700

    Refactor Details render

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -23,49 +23,44 @@ class Details extends Component {
     .catch(err => this.setState({error: true}));
   }
 
-  render() {
-    if (this.state.error) {
-      return <h1>More shit...</h1>
-    }
-
-    if (this.state.movie) {
-      return (
-        <>
-          <div
-            className="details-wrapper"
-            style={
-              {backgroundImage: `url(${this.state.movie.backdrop_path})`}
-            }>
-          </div>
-          <div className="details-modal">
-            <img className="details-poster" src={`${this.state.movie.poster_path}`} />
-            <section className="details-info">
-              <h2 className="details-title">{this.state.movie.title}</h2>
-              <p className="details-release-date">{this.state.movie.release_date.slice(0,4)}</p>
-              <p className="details-overview__heading">Overview</p>
-              <p className="details-overview">{this.state.movie.overview}</p>
-              <p className="details-genre__heading">Genres</p>
-              <p className="details-genre">{this.state.movie.genres.join(', ')}</p>
-              <div className="details-bottom">
-                <div className="details-trivia-wrapper">
-                  <p className="details-trivia__heading">Trivia</p>
-                  <p className="details-trivia-fact">Budget: ${this.state.movie.budget}</p>
-                  <p className="details-trivia-fact">Runtime: {this.state.movie.runtime}mins</p>
-                  <p className="details-trivia-fact">Revenue: ${this.state.movie.revenue}</p>
-                </div>
-                <div className="details-rating-wrapper">
-                  <p className="details-rating">{this.state.movie.average_rating.toFixed(1)}</p>
-                </div>
+  render = () => (
+    this.state.error ? <h1>More shit...</h1>
+    : this.state.movie ? (
+      <>
+        <div
+          className="details-wrapper"
+          style={
+            {backgroundImage: `url(${this.state.movie.backdrop_path})`}
+          }>
+        </div>
+        <div className="details-modal">
+          <img className="details-poster" src={`${this.state.movie.poster_path}`} />
+          <section className="details-info">
+            <h2 className="details-title">{this.state.movie.title}</h2>
+            <p className="details-release-date">{this.state.movie.release_date.slice(0,4)}</p>
+            <p className="details-overview__heading">Overview</p>
+            <p className="details-overview">{this.state.movie.overview}</p>
+            <p className="details-genre__heading">Genres</p>
+            <p className="details-genre">{this.state.movie.genres.join(', ')}</p>
+            <div className="details-bottom">
+              <div className="details-trivia-wrapper">
+                <p className="details-trivia__heading">Trivia</p>
+                <p className="details-trivia-fact">Budget: ${this.state.movie.budget}</p>
+                <p className="details-trivia-fact">Runtime: {this.state.movie.runtime}mins</p>
+                <p className="details-trivia-fact">Revenue: ${this.state.movie.revenue}</p>
               </div>
-              <Link className="back-button" to='/' onClick={this.props.clearCurrentMovie}>Back to Movies</Link>
-            </section>
-          </div>
-        </>
-    )} else {
-      return <DetailsLoader />
-    }
-  }
+              <div className="details-rating-wrapper">
+                <p className="details-rating">{this.state.movie.average_rating.toFixed(1)}</p>
+              </div>
+            </div>
+            <Link className="back-button" to='/' onClick={this.props.clearCurrentMovie}>Back to Movies</Link>
+          </section>
+        </div>
+      </>
+    ) : (
+      <DetailsLoader />
+    )
+  )
 }
-
 
 export default Details;
