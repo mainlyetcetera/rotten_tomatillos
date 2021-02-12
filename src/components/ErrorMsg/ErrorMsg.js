83aa66ae59dba4a92d4d1c2a43cb8597b7542f67
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-12 10:05:57 -0700

    Remove comments from ErrorMsg file

--- src/components/ErrorMsg/ErrorMsg.js
+++ src/components/ErrorMsg/ErrorMsg.js
@@ -6,14 +6,3 @@ export const ErrorMsg = () => (
     <h3 className='later-text'>Please try again later!</h3>
   </div>
 )
-
-// centered
-// in a box
-// rounded corners
-// coloring that works with the darker background
-// a little shadowing under the box, on the left, right, bottom
-
-// link centered under the box
-// therefore I think the container should handle the styling for this box
-// that way it can put the error and link together
-// eventually this link should be in the box
