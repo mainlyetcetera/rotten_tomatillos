7f0588bb93508bfe5ec3d0eeeaa03dca8cca462a
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-11 17:39:05 -0700

    Add necessary imports

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { ErrorMsg } from '../ErrorMsg/ErrorMsg.js';
 import DetailsLoader from '../DetailsLoader/DetailsLoader.js'
+import DetailsModal from '../DetailsModal/DetailsModal.js'
 import './Details.css'
 import fetchData from '../../apiCalls.js';
 
--- src/components/DetailsModal/DetailsModal.js
+++ src/components/DetailsModal/DetailsModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './DetailsModal.css'
+import { Link } from 'react-router-dom';
 
 const DetailsModal = ( { movie }) => {
 
