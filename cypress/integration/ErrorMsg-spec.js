7952213761912e5e8678e6cecd3b5f6460660a05
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-12 10:15:41 -0700

    Actually add the test skeleton for ErrorMsg...

--- cypress/integration/ErrorMsg-spec.js
+++ cypress/integration/ErrorMsg-spec.js
@@ -1,5 +1,18 @@
 describe('error messages', () => {
   it('should not have to visit the page alone', () => {
+    // we do not have to have a cy.visit here if we run all the tests at once
     expect(true).to.equal(true);
+  });
+
+  it('should mainly be a div', () => {
+    
+  });
+
+  it('should contain "Sorry, something went wrong!"', () => {
+    
+  });
+
+  it('should contain "Please try again later!"', () => {
+    
   });
 });
