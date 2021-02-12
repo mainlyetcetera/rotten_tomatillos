237fe7c3220b4e3c3e53e56d0fee528eee533f25
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-12 10:04:25 -0700

    Add first test skeleton to App test file

--- cypress/integration/App-spec.js
+++ cypress/integration/App-spec.js
@@ -1,10 +1,31 @@
-describe('test', () => {
-  it('should exist', () => {
-    expect(true).to.equal(true);
+describe('the App component', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000');
   });
 
-  it('should be able to find the site', () => {
-    cy.visit('localhost:3000');
+  it('should render a header', () => {
+
+  });
+
+  it('should render movies', () => {
+
+  });
+
+  it('should render a single movie from the url', () => {
+    // cy.visit a single page here
+
+  });
+
+  it('should be able to fetch movies', () => {
+
+  });
+
+  it('should be able to fetch a single movie', () => {
+
+  });
+
+  it('should render an error message if the fetch fails', () => {
+
   });
 });
 
