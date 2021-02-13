067444a4ebd9e8f6d0dd04f51915f0a9eca3ab86
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-13 10:12:10 -0700

    Move single-movie view tests to new describe

--- cypress/integration/App-spec.js
+++ cypress/integration/App-spec.js
@@ -1,33 +1,39 @@
-describe('the App component', () => {
+describe('the main poster view', () => {
   beforeEach(() => {
     cy.visit('localhost:3000');
   });
 
   it('should render a header', () => {
-    cy.get('header')
+    cy
+      .get('header')
       .contains('Rotten Tomatillos');
   });
 
   it('should render movies', () => {
-
+    // check class name?
+    // check how many there are?
+    // check that there are multiple articles
+    cy.get('section')
   });
 
-  it('should render a single movie from the url', () => {
-    // cy.visit a single page here
-
-  });
 
   it('should be able to fetch movies', () => {
 
   });
 
-  it('should be able to fetch a single movie', () => {
-
-  });
 
   it('should render an error message if the fetch fails', () => {
 
   });
 });
 
+describe('the single movie view'), () => { 
+  it('should render a single movie from the url', () => {
+    // cy.visit a single page here
 
+  });
+
+  it('should be able to fetch a single movie', () => {
+
+  });
+});
