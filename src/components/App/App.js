c5f8d80e0513cb516dc88f7884f2c19c22e5d988
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-14 11:56:57 -0700

    Responsively style grid

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   }
 
   componentDidMount = () => {
-    fetchData('https://rancid-tomatillos.herokuapp.com/api/v2/movie')
+    fetchData('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
     .then(data => setTimeout(() =>
        this.setState({
          movies: data.movies
