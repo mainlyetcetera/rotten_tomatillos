d550d47f0b9100e9b2abcc516a7053b99bca9388
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-14 13:05:01 -0700

    Undo changes I don't want to make

--- src/components/ErrorMsg/ErrorMsg.js
+++ src/components/ErrorMsg/ErrorMsg.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './ErrorMsg.css'
 
-export const ErrorMsg = () => (
+export const ErrorMsg = (props) => (
   <div className='error-wrapper'>
     <h2 className='error-msg'>Sorry, something went wrong!</h2>
-    <h3>Please try again later!</h3>
+    <h3>{props.message}</h3>
   </div>
 )
