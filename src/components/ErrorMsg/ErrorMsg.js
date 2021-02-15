bf512cc2f79a35ea66955a3c65ce63ccddace6ea
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-15 09:56:42 -0700

    Pull dynamic code out of Error

--- src/components/ErrorMsg/ErrorMsg.js
+++ src/components/ErrorMsg/ErrorMsg.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './ErrorMsg.css'
 
-export const ErrorMsg = (props) => (
+export const ErrorMsg = () => (
   <div className='error-wrapper'>
     <h2 className='error-msg'>Sorry, something went wrong!</h2>
-    <h3>{props.message}</h3>
+    <h3>Please try again later.</h3>
   </div>
 )
