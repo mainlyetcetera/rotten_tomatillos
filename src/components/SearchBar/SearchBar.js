7944fed2efc64a6133fc2bdc0dd411502168f291
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-15 10:28:05 -0700

    Build SearchBar class component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return <input className="search-bar"></input>
+  }
+}
+
+export default SearchBar
